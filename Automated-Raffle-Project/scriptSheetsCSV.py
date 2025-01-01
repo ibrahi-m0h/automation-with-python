ed1eec72425d337af3d1c6498788849286cc871b
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-01-01 12:34:07 -0800

    Create a CSV reading script and extract email column into a list

---
+++ Automated-Raffle-Project/scriptSheetsCSV.py
@@ -0,0 +1,14 @@
+# This program is meant to extract a list of emails from a Google Sheets csv file and return a random email for a raffle
+
+import pandas as pd 
+
+# Read csv file into dataframe
+tables = pd.read_csv('BTG End of the Year Survey 2024-2025 (Responses) - Form Responses 1.csv')
+
+#Extract email column and instantiate into a variable
+email_column = tables['Email']
+
+#Convert email_column objects into an array
+email_List = list(email_column)
+
+print(email_List)
